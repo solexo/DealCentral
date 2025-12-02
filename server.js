fb1ae55b5f4b7cad94b269d681d42b8776358c2f
User <user@example.com>
2025-12-02 20:23:06 +0100

    Restore all products with correct image URLs

--- server.js
+++ server.js
@@ -15,12 +15,12 @@ const products = [
   {
     "name": "Wireless Bluetooth Headphones",
     "url": "https://example.com/product1",
-    "image": "https://images-na.ssl-images-amazon.com/images/I/71qid7QFWJL._AC_SL1500_.jpg"
+    "image": "https://m.media-amazon.com/images/I/71qid7QFWJL._AC_SL1500_.jpg"
   },
   {
     "name": "Apple iPhone 14 Pro",
     "url": "https://example.com/product2",
-    "image": "https://images-na.ssl-images-amazon.com/images/I/61XO4bORHVL._AC_SL1500_.jpg"
+    "image": "https://m.media-amazon.com/images/I/61XO4bORHVL._AC_SL1500_.jpg"
   },
   {
     "name": "Hairmax PowerFlex 272 Laser Cap - (FDA Cleared) Thinning Hair Loss Solution for Men & Women - Flexible Cordless Design With 7 Minute Treatment Time - Red Light Therapy to Stimulate Hair Growth",
