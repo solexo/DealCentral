d2b05ff0bfda372bbf552540b20ee8bbbe471ec5
User <user@example.com>
2025-12-02 20:21:19 +0100

    Fix static files path for Vercel deployment

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
